fe2030f675a4cb45c341398739f128b1566551a5
Philipp <fsfeel@gmail.com>
2012-06-25 20:24:19 +0400

    add utils.round

--- src/mistress/utils.lua
+++ src/mistress/utils.lua
@@ -319,4 +319,14 @@ function _M.sub (s, tab)
 end
 
 
+function _M.round (num, idp)  -- from http://lua-users.org/wiki/SimpleRound
+	local mult = 10^(idp or 0)
+	if num >= 0 then
+		return math.floor(num * mult + 0.5) / mult
+	else
+		return math.ceil(num * mult - 0.5) / mult
+	end
+end
+
+
 return _M
